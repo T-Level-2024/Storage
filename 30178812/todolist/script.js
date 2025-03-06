5941b8ba24876140b094ad5d8daa197628bc913b
30178812 <147609790+30178812@users.noreply.github.com>
2025-03-06 14:00:17 +0000

    Better

--- 30178812/todolist/script.js
+++ 30178812/todolist/script.js
@@ -13,48 +13,39 @@ let tasks = [];
 
 // make a function calle updateTaskList()
 
-function removeEventListeners() {
-    let element;
-    for (let i = 0; i<tasks.length; i++) {
-        element = document.getElementById(`L${i}R`);
-        if(element) {
-            element.replaceWith(element.cloneNode(true));
-            console.log(`Replaced L${i}R`)
-        }
+list.addEventListener("click", (event) => {
+    // Checks what the element ID ends with:
+    // R for remove
+    // E for edit
+    // S for submit (comes up after edit is pressed)
+    let target = event.target;
+    let element = target.id;
+    let index = element.charAt(1);
+    if (element.endsWith("R")) {
+        removeItem(index);
+    } else if (element.endsWith("E")) {
+        enableEditing(index);
+    } else if (element.endsWith("S")) {
+        replaceItem(index, document.getElementById(`L${index}I`).value);
     }
-}
+})
 
 function updateTaskList() {
-    if (list.innerHTML !== "") {
-        removeEventListeners()
-        list.innerHTML = "";
-    }
+    list.innerHTML = "";
     console.log(tasks);
-    let newbutton;
     let index;
     for (let i=0; i<tasks.length; i++) {
         index = i;
         console.log(`updateTaskList(): I(${index}) task(${tasks[index]})`);
-        if(tasks[i][1]){ // Input box, submit, remove
-            list.innerHTML += `<li id="L${index}"><input id="L${index}I" type="text" placeholder="New text here"><button id="L${index}S">Submit</button><button id="L${index}R">Remove</button></li>`;
-            newbutton = document.getElementById(`L${index}S`);
-            setInterval(50);
-            console.log(newbutton);
-            newbutton.addEventListener("click", () => replaceItem(Number(`${index}`), document.getElementById(`L${index}I`).value));
-            console.log("updateTaskList(): Submit event listener applied");
-        } else { // Item, edit, remove
+        if(tasks[i][1]){ 
+            // Input box, submit, remove
+            // Any existing text is moved to the input box.
+            list.innerHTML += `<li id="L${index}"><input id="L${index}I" type="text" placeholder="New text here" value="${tasks[index][0]}"><button id="L${index}S">Submit</button><button id="L${index}R">Remove</button></li>`;
+        } else { 
+            // Item, edit, remove
+            // User can either edit the item text or remove the item entirely
             list.innerHTML += `<li id="L${i}">${tasks[index][0]} <button id="L${index}E">Edit</button><button id="L${index}R">Remove</button></li>`;
-            newbutton = document.getElementById(`L${index}E`);
-            setInterval(50);
-            console.log(newbutton);
-            newbutton.addEventListener("click", () => enableEditing(Number(`${index}`)));
-            console.log("updateTaskList(): Edit event listener applied");
         };
-        newbutton = document.getElementById(`L${index}R`);
-        setInterval(50);
-        console.log(newbutton);
-        newbutton.addEventListener("click", () => removeItem(Number(`${index}`)));
-        console.log("updateTaskList(): Remove event listener applied");
     };
 };
 
@@ -67,37 +58,23 @@ function enableEditing(element) {
 // print to the console 
 
 function replaceItem(element, text) {
-    console.log(`L${element}: ${text}`);
+    console.log(`L${element}: ${tasks[element][0]} --> ${text}`);
     tasks[element][0] = text;
     tasks[element][1] = false;
     updateTaskList();
 }
 
 function removeItem(element) {
-    console.log("removeItem() called");
     tasks.splice(element, 1);
     updateTaskList();
 }
 
 addbutton.addEventListener("click", () => {
     tasks.push([input.value, false]);
+    input.value = "";
     updateTaskList();
 });
 
-
-
-
-
-
-
-
-
-
-//addbutton.addEventListener("click", () => {
-//    //list.innerHTML += `<li>${input.value}</li>`
-//    list.innerHTML += `<li id="${input.value}">${input.value} <button onClick="document.getElementById('list').removeChild(document.getElementById('${input.value}'))">Remove</button></li>`;
-//});
-//
 removebutton.addEventListener("click", () => {
     //list.removeChild(list.lastElementChild);
     list.innerHTML = ""
